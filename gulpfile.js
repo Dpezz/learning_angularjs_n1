8371e4ada8ea85e517ae5f23c98d0dc73a512b05
Daniel Jara P <djara@corebox.cl>
2017-08-22 23:44:46 -0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var connect = require('gulp-connect');
+// var inject = require('gulp-inject');
+var wiredep = require('wiredep').stream;
+// var notify = require("gulp-notify");
+
+gulp.task('webserver', function () {
+  gulp.src('app')
+    .pipe(webserver({
+      port: '3000',
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
+});
+
+// gulp.task('inject', function () {
+//   var sources = gulp.src(['./app/js/**/*.js', './app/css/**/*.css']);
+//   return gulp.src('index.html', {
+//       cwd: './app/'
+//     })
+//     .pipe(inject(sources, {
+//       read: false,
+//       ignorePath: '/app/',
+//       relative: true
+//     }))
+//     .pipe(gulp.dest('./app/'));
+// });
+
+
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/**/*.html'], ['html']);
+  gulp.watch(['./app/js/**/*.js'], ['js']);
+  gulp.watch(['./app/css/**/*.css'], ['css']);
+  gulp.watch(['./bower.json'], ['wiredep']);
+});
+
+
+gulp.task('default', ['webserver']);
